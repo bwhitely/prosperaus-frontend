535a4a163d9a4198e223928559d08f3bb57a542d
Ben Whitely <bwhitely@live.com>
2026-01-03 18:22:41 +1030

    update prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://api.ausfinance.app/api/v1',
+  apiBaseUrl: 'https://prosperaus-backend-production.up.railway.app/api/v1',
   supabaseUrl: 'https://mbekewpdnodbixtdijek.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1iZWtld3Bkbm9kYml4dGRpamVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY2MDIxMzIsImV4cCI6MjA4MjE3ODEzMn0.mnxr9RUnJx7gLx_65wUBm52C7V49KCqz5f-2ytRear0'
 };
